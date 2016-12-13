d4253ae08185840a7182186b814c78fccc923828
Angel Condori <angel.condori@gmail.com>
2016-12-12 20:13:55 -0500

    Archivo bases

---
+++ index.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+	<meta charset="UTF-8">
+	<title>SiSAgencia</title>
+</head>
+<body>
+	<header>
+		<div class="titulo">
+			<h1>Agencia de Turismo</h1>
+		</div>
+		<div class="logo">
+			<img src="recursos/img/logo.jpg" alt="Logo">
+		</div>
+	</header>
+	<nav>
+		<ol>
+			<li><a href="#">Inicio</a></li>
+			<li><a href="#">Itinerarios</a></li>
+			<li><a href="#">Contáctenos</a></li>
+		</ol>
+	</nav>
+	<section>
+		<div class="mensaje">
+			<div class="mtitulo">
+				<h2>Planea tu viaje</h2>
+			</div>
+			<div class="texto">
+				<p>Puedes planear tu viaje a cualquier parte del Perú, haciendo clic en los enlaces de nuestra página</p>
+			</div>
+		</div>
+		<div class="slider"></div>
+	</section>
+	<footer></footer>
+</body>
+</html>
